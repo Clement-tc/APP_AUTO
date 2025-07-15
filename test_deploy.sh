80cb36ad7333ce0c73e981d346ad765e4fecf625
eliot ceron <eliot-arana@outlook.com>
2025-07-15 15:20:59 +0200

    updt modify release

--- test_deploy.sh
+++ test_deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 minikube start 
+helm repo add kubecost https://kubecost.github.io/cost-analyzer/
+helm repo update
+
+kubectl create namespace kubecost
+
+helm install kubecost kubecost/cost-analyzer \
+  --namespace kubecost \
+  --set kubecostToken="1234567890abcdef" \
+  --set global.prometheus.enabled=false \
+  --set global.prometheus.fqdn="http://prometheus.monitoring.svc.cluster.local"
+
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+helm repo update
+
+kubectl create namespace monitoring
+
+helm install kube-prom-stack prometheus-community/kube-prometheus-stack --namespace monitoring
 
 
 eval $(minikube docker-env)
